c56f83fdebe4f78c67ceaf525a687d30cde911bc
Neil Kelsey <neil.kelsey@gmail.com>
2024-04-20 13:38:54 +0100

    feat: Adding text case change functionality

--- text-case-change/src/App.js
+++ text-case-change/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [textValue, setTextValue] = useState('');
+  const [styleType, setStyleType] = useState('');
+
+  const textChangeClickHandler = (event) => {
+    setTextValue(event.target.value);
+  }
+
+  const styleChangeClickHandler = (event) => {
+    const styleType = event.target.getAttribute('style-type');
+
+    console.log('NeilTest - styleType', styleType);
+
+    styleType === 'remove' ? console.log('NeilTest - true') : console.log('NeilTest - false');
+    styleType === 'remove' ? setStyleType('remove') : <></>;
+    styleType === 'uppercase' ? setStyleType('uppercase') : <></>;
+    styleType === 'lowercase' ? setStyleType('lowercase') : <></>;
+    styleType === 'capitalization' ? setStyleType('capitalization') : <></>;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Text case change</h1>
+      <label>Enter your text </label>
+      <input onChange={textChangeClickHandler} />
+      <p className={styleType}>{textValue}</p>
+      <button onClick={styleChangeClickHandler} style-type="remove">Remove styling</button>
+      <button onClick={styleChangeClickHandler} style-type="uppercase">uppercase</button>
+      <button onClick={styleChangeClickHandler} style-type="lowercase">lowercase</button>
+      <button onClick={styleChangeClickHandler} style-type="capitalization">capitalization</button>
     </div>
   );
 }
